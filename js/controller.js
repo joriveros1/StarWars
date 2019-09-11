29085238116a3cf4439d49e49ba45fdd26dee2ad
firebotco2019 <sigueme.android@gmail.com>
2019-09-11 18:08:47 -0500

    StarWars

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,15 @@
 var app = angular.module("StarWars", []);
-app.controller("SkyWalker",function($scope){
- $scope.titulo = "Tienda StarWars";
+app.controller("SkyWalker", function($scope, $http) {
+    $scope.paginas = {};
+    $scope.titulo = "Tienda StarWars";
+    $http.get("https://swapi.co/api/people/?format=json")
+        .success(function(data) {
+            $scope.data = data;
+            angular.forEach($scope.data, function(persona) {
+                $scope.paginas[persona] = '';
+            });
+        })
+        .error(function(err) {
+
+        });
 });
