fed3324a57a1ecc44f47c6a1eb6430a9aed35f69
firebotco2019 <sigueme.android@gmail.com>
2019-09-11 19:54:36 -0500

    StarWars

--- js/controller.js
+++ js/controller.js
@@ -3,8 +3,12 @@ app.controller("SkyWalker", function($scope, $http) {
     var url = 'https://swapi.co/api/';
     $scope.paginas = [
         { pagina: "Personajes", url: "personajes.html" },
-        { pagina: "Planetas", url: "planetas.html" }
+        { pagina: "Planetas", url: "planetas.html" },
+        { pagina: "Peliculas", url: "peliculas.html" },
+        { pagina: "Especies", url: "especies.html" },
+        { pagina: "Vehiculos", url: "vehiculos.html" },
+        { pagina: "Naves", url: "naves.html" }
     ];
     $scope.titulo = "Tienda StarWars";
-    
+
 });
